f0e4494090cb05b3071063d651f61411d355847c
s.zenkov <s.zenkov@corp.mail.ru>
2021-04-29 10:45:41 +0300

    add SelectWithMouseViaUniRx raw implementation

--- Assets/Scripts/SelectObjects/SelectWithMouse/SelectWithMouseViaUniRx.cs
+++ Assets/Scripts/SelectObjects/SelectWithMouse/SelectWithMouseViaUniRx.cs
@@ -1,10 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
+using SelectObjects;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SelectWithMouseViaUniRx : MonoBehaviour
+public class SelectWithMouseViaUniRx : EmptyGraphic
 {
-    private void Awake() {
+    [SerializeField] private SelectionFrameBase _selectionFrame;
+    private Vector3 _beginDragPosition;
+    private Vector3 _endDragPosition;
+    private Rect _selectedRect;
+
+    private ISelectionCoordinatesConverter _selectionCoordinatesConverter;
+    
+    private void Awake()
+    {
+        _selectionCoordinatesConverter = new ScreenSpaceSelectionCoordinatesConverter(Camera.main);
+    }
+    
+    private void Start()
+    {
+        this.OnBeginDragAsObservable()
+            .Select(_ => Input.mousePosition)
+            .RepeatUntilDestroy(this)
+            .Subscribe(position =>
+            {
+                _beginDragPosition = position;
+                _selectedRect = Rect.zero;
+                _selectionFrame.UpdateRect(_selectedRect);
+            });
         
+        this.OnDragAsObservable()
+            .TakeUntil(this.OnEndDragAsObservable())
+            .Select(_ => Input.mousePosition)
+            .RepeatUntilDestroy(this)
+            .Subscribe((unit =>
+            {
+                _endDragPosition = unit;
+                UpdateSelectedRect();
+                _selectionFrame.UpdateRect(_selectedRect);
+            }), () => { });
+
+        this.OnEndDragAsObservable()
+            .Select(_ => Input.mousePosition)
+            .RepeatUntilDestroy(this)
+            .Subscribe(position =>
+            {
+                _selectedRect = Rect.zero;
+                _selectionFrame.UpdateRect(_selectedRect);
+            });
+    }
+    
+    private void UpdateSelectedRect()
+    {
+        _selectedRect = _selectionCoordinatesConverter.GetSelectionRect(_beginDragPosition, _endDragPosition);
+        Camera camera = Camera.main;
+
+        foreach (var selectable in Selectables.SelectableObjects)
+        {
+            bool selected = selectable.GetSelectableChecker().CheckSelected(camera, _selectedRect, transform, _selectionCoordinatesConverter);
+            selectable.SetSelected(selected);
+        }
     }
 }
